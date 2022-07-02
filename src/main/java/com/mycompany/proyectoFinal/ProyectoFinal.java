56fc164c38e8c7534c99bb134105280a8758cdd7
Dino Roca <rocalauented@gmaiil.com>
2022-07-01 20:53:36 -0500

    Hola mundo

--- src/main/java/com/mycompany/proyectoFinal/ProyectoFinal.java
+++ src/main/java/com/mycompany/proyectoFinal/ProyectoFinal.java
@@ -18,5 +18,6 @@ public class ProyectoFinal {
         
         System.out.println("La suma es: "+ consulta);
         System.out.println("Holaaa mundo");
+        System.out.println("Hola mundo x2");
     }
 }
