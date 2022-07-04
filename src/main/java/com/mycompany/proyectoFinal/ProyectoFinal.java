18e4c9d3086a4a796f98b73b924da19d2e1257d7
Dino Roca <rocalauented@gmaiil.com>
2022-07-04 09:56:49 -0500

    a

--- src/main/java/com/mycompany/proyectoFinal/ProyectoFinal.java
+++ src/main/java/com/mycompany/proyectoFinal/ProyectoFinal.java
@@ -11,13 +11,8 @@ public class ProyectoFinal {
         System.out.println(t1+ " " + (q1.hasMoreSolutions()? "Satisfactorio" : "No se pudo acceder"));
         
         System.out.println("-----Iniciando suma----");
-<<<<<<< HEAD
-
-        String suma = "suma(9, 10, C).";
-=======
         
         String suma = "suma(10, 10, C).";
->>>>>>> fb14ea77cd91ad9f1b6fc8c2fba3c4678aabb253
         Query q2 = new Query(suma);
         String consulta = q2.oneSolution().get("C").toString();
         
