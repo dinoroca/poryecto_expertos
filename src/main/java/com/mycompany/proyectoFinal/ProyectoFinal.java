344262b0c4ff04a02b9161eac2375c70bb6b9085
juniorPa <56480463+juniorPa@users.noreply.github.com>
2022-07-15 10:42:55 -0500

    Revert "Merge pull request #1 from dinoroca/develop"
    This reverts commit f2d73686112faab051156782f229e891e3f247c8.

--- src/main/java/com/mycompany/proyectoFinal/ProyectoFinal.java
+++ src/main/java/com/mycompany/proyectoFinal/ProyectoFinal.java
@@ -5,20 +5,19 @@ import org.jpl7.Query;
 
 public class ProyectoFinal {
     public static void main(String[] args) {
-        String t1 = "consult('conocimiento.pl')";
+        String t1 = "consult('prueba.pl')";
         
         Query q1 = new Query(t1);
         System.out.println(t1+ " " + (q1.hasMoreSolutions()? "Satisfactorio" : "No se pudo acceder"));
         
         System.out.println("-----Iniciando suma----");
         
-        String suma = "main.";
+        String suma = "suma(9, 10, C).";
         Query q2 = new Query(suma);
-        //String consulta = q2.oneSolution().get("C").toString();
+        String consulta = q2.oneSolution().get("C").toString();
         
-        //System.out.println("La suma es: "+ consulta);
+        System.out.println("La suma es: "+ consulta);
         System.out.println("Holaaa mundo");
-        System.out.println("hola ddd");
-        System.out.print("hola");
+        System.out.println("Hola mundo x2");
     }
 }
